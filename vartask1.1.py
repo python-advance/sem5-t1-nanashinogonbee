a421cb4f67b644c571e89da7631402f378160655
nanashinogonbee <31703502+nanashinogonbee@users.noreply.github.com>
2020-06-28 20:12:44 +0300

    Prettify

--- vartask1.1.py
+++ vartask1.1.py
@@ -3,7 +3,8 @@ import requests
 
 class RemoteImport:
     def __init__(self):
-        pass
+        print('initialized')
+
 
     def pypi_find(self, query):
         ENDPOINT = 'https://pypi.org/search/'
@@ -13,23 +14,23 @@ class RemoteImport:
         results = soup.find_all('ul', {'class': 'unstyled'})
         results = [i.find_all('li') for i in results][0]
 
-        pkg = 'package-snippet_'
-
-        package_names = [i.find('span', {'class': f'{pkg}_name'}).text for i in results]
-        package_vers =[i.find('span', {'class': f'{pkg}_version'}).text for i in results]
+        package_names = [i.find(
+            'span', {'class': 'package-snippet__name'}
+            ).text for i in results]
+        package_vers = [i.find(
+            'span', {'class': 'package-snippet__version'}
+            ).text for i in results]
 
         result = list(zip(package_names, package_vers))
-        result = '\n'.join([f'{i[0]} {i[1]}' for i in result])
+        result = '\n'.join([' '.join(i) for i in result])
 
-        print(f'''Query: {query}
-----------
-Found:
-{result}''')
+        print('Query: {}\n----------\nFound:\n{}'.format(query, result))
+
 
     def pypi_import(search, package):
-        os.system('pip install {package}')
+        os.system('pip install {}'.format(package))
 
 
 importer = RemoteImport()
 importer.pypi_find(input('find: '))
-# importer.pypi_import('')
+# importer.pypi_import()
